fcf5dff10c6e3b7416191249fe976af4b9b63f98
monika2014 <monikarmtr@gmail.com>
2023-03-06 16:43:19 +0530

    Add files via upload

---
+++ C180_speech_to_text.py
@@ -0,0 +1,25 @@
+# pip install SpeechRecognition
+# python -m pip install -U pip
+
+# pip install winspeech
+
+# pip3 install pyaudio
+
+from tkinter import *
+import speech_recognition as sr
+root = Tk()
+root.geometry("500x500")
+
+def r_audio():
+    speech_recognisor= sr.Recognizer()
+    with sr.Microphone() as source:
+        audio=  speech_recognisor.listen(source)
+        voice_data=''
+        try:
+            voice_data=  speech_recognisor.recognize_google(audio, language='en-in')
+        except sr.UnknownValueError:
+            print('Please repeat i did not get that')
+    print(voice_data)
+    
+r_audio()
+root.mainloop()
